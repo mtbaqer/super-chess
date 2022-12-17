f2c879e0c992cae1001dd78b6fa13d38a8e0b08e
Mohammad Baqer <mtbaqer@gmail.com>
2022-12-16 22:31:46 -0500

    update schema

--- types/supabase.ts
+++ types/supabase.ts
@@ -1,62 +1,103 @@
-export type Json = string | number | boolean | null | { [key: string]: Json } | Json[];
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
 
 export interface Database {
   public: {
     Tables: {
       cells: {
         Row: {
-          room_id: number;
-          index: number;
-        };
+          room_id: number
+          row: number
+          column: number
+          id: number
+        }
         Insert: {
-          room_id: number;
-          index: number;
-        };
+          room_id: number
+          row: number
+          column: number
+          id?: number
+        }
         Update: {
-          room_id?: number;
-          index?: number;
-        };
-      };
+          room_id?: number
+          row?: number
+          column?: number
+          id?: number
+        }
+      }
+      objects: {
+        Row: {
+          id: number
+          cell_id: number | null
+          breakable: boolean
+          modifier: string | null
+          type: string
+        }
+        Insert: {
+          id?: number
+          cell_id?: number | null
+          breakable: boolean
+          modifier?: string | null
+          type: string
+        }
+        Update: {
+          id?: number
+          cell_id?: number | null
+          breakable?: boolean
+          modifier?: string | null
+          type?: string
+        }
+      }
       players: {
         Row: {
-          id: number;
-          created_at: string | null;
-          room_id: number | null;
-        };
+          id: number
+          created_at: string | null
+          room_id: number | null
+          modifiers: string[]
+        }
         Insert: {
-          id?: number;
-          created_at?: string | null;
-          room_id?: number | null;
-        };
+          id?: number
+          created_at?: string | null
+          room_id?: number | null
+          modifiers: string[]
+        }
         Update: {
-          id?: number;
-          created_at?: string | null;
-          room_id?: number | null;
-        };
-      };
+          id?: number
+          created_at?: string | null
+          room_id?: number | null
+          modifiers?: string[]
+        }
+      }
       rooms: {
         Row: {
-          id: number;
-          created_at: string | null;
-        };
+          id: number
+          created_at: string | null
+          temp: number | null
+        }
         Insert: {
-          id?: number;
-          created_at?: string | null;
-        };
+          id?: number
+          created_at?: string | null
+          temp?: number | null
+        }
         Update: {
-          id?: number;
-          created_at?: string | null;
-        };
-      };
-    };
+          id?: number
+          created_at?: string | null
+          temp?: number | null
+        }
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Enums: {
-      [_ in never]: never;
-    };
-  };
+      [_ in never]: never
+    }
+  }
 }
