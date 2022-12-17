e0288a30d0474e4f8d56e9e6e00791420ea5bf79
Mohammad Baqer <mtbaqer@gmail.com>
2022-12-17 18:17:08 -0500

    show unbreakable blocks as knight

--- components/Board/GameObject.tsx
+++ components/Board/GameObject.tsx
@@ -1,17 +1,14 @@
 import React, { FunctionComponent } from "react";
 import { GameObject } from "../../types/types";
 import Bishop from "./pieces/Bishop";
+import Knight from "./pieces/Knight";
 
 interface Props {
   gameObject: GameObject;
 }
 
 const GameObject: FunctionComponent<Props> = ({ gameObject }) => {
-  return (
-    <div className="">
-      <Bishop />
-    </div>
-  );
+  return <div className="">{gameObject.breakable ? <Bishop /> : <Knight />}</div>;
 };
 
 export default GameObject;
